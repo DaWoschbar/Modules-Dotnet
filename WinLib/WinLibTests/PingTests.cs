1270f420f52fa10896583ca43cd2196153562068
Caleb McGary <intern4tional@users.noreply.github.com>
2019-10-06 17:19:50 -0700

    Adding in PortScan Tests

--- WinLib/WinLibTests/PingTests.cs
+++ WinLib/WinLibTests/PingTests.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Faction.Modules.Dotnet.Common;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-using Faction.Modules.Dotnet.Common;
-using Faction.Modules.Dotnet.Commands;
+using System;
+using System.Collections.Generic;
 
 namespace WinLibTests
 {
---
+++ WinLib/WinLibTests/PortScanTests.cs
@@ -0,0 +1,40 @@
+﻿using Faction.Modules.Dotnet.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace WinLibTests
+{
+    [TestClass]
+    public class PortScanTests
+    {
+        Dictionary<string, string> Parameters = new Dictionary<string, string>();
+        [TestMethod]
+        public void PortScanInitialize()
+        {
+            List<Command> commands = Faction.Modules.Dotnet.Initialize.GetCommands();
+            Faction.Modules.Dotnet.Common.Command pingCommand = commands[1];
+            Assert.AreEqual("portscan", pingCommand.Name);
+        }
+
+        [TestMethod]
+        public void PortScanTimeOut()
+        {
+        }
+
+        [TestMethod]
+        public void PortScanThreads()
+        {
+        }
+
+        [TestMethod]
+        public void PortScanPing()
+        {
+        }
+
+        [TestMethod]
+        public void PortScanMultipleComputers()
+        {
+        }
+    }
+}
