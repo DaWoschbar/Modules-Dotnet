9665473576bc3636515ed8f92f49a270310f458c
Caleb McGary <intern4tional@users.noreply.github.com>
2019-10-07 22:13:14 -0700

    adding in registry tests / finishing powershell tests

--- WinLib/WinLibTests/PowerShellTests.cs
+++ WinLib/WinLibTests/PowerShellTests.cs
@@ -19,6 +19,9 @@ namespace WinLibTests
             Assert.AreEqual("powershell", powershellCommand.Name);
         }
 
+        /// <summary>
+        /// This does not properly test the bypass logging at this time
+        /// </summary>
         [TestMethod]
         public void PowershellBypassLogging()
         {
@@ -31,20 +34,32 @@ namespace WinLibTests
             
         }
 
+        /// <summary>
+        /// This does not properly test bypassing amsi at this time
+        /// </summary>
         [TestMethod]
         public void PowershellBypassAMSI()
         {
             List<Command> commands = Faction.Modules.Dotnet.Initialize.GetCommands();
             Faction.Modules.Dotnet.Common.Command powershellCommand = commands[2];
-            Assert.AreEqual("powershell", powershellCommand.Name);
+            Parameters.Add("Command", "Test-NetConnection 127.0.0.1");
+            Parameters.Add("BypassAmsi", "true");
+            EventLog log = new EventLog("Security");
+            CommandOutput results = powershellCommand.Execute(Parameters);
         }
 
+        /// <summary>
+        /// This does not properly test outstring at this time
+        /// </summary>
         [TestMethod]
         public void PowershellOutString()
         {
             List<Command> commands = Faction.Modules.Dotnet.Initialize.GetCommands();
             Faction.Modules.Dotnet.Common.Command powershellCommand = commands[2];
-            Assert.AreEqual("powershell", powershellCommand.Name);
+            Parameters.Add("Command", "Test-NetConnection 127.0.0.1");
+            Parameters.Add("OutString", "true");
+            EventLog log = new EventLog("Security");
+            CommandOutput results = powershellCommand.Execute(Parameters);
         }
     }
 }
---
+++ WinLib/WinLibTests/RegTests.cs
@@ -0,0 +1,61 @@
+﻿using Faction.Modules.Dotnet.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace WinLibTests
+{
+    [TestClass]
+    public class RegTests
+    {
+        /// <summary>
+        /// Scaffolding Method for RegistryWrite and RegistryDelete Tests
+        /// </summary>
+        private CommandOutput KeyOperations(string operation, string path, string value)
+        {
+            Dictionary<string, string> Parameters = new Dictionary<string, string>();
+            List<Command> commands = Faction.Modules.Dotnet.Initialize.GetCommands();
+            Faction.Modules.Dotnet.Common.Command regCommand = commands[3];
+            Parameters.Add("Operation", operation);
+            Parameters.Add("Path", path);
+            if (value!=String.Empty)
+            {
+                Parameters.Add("Value", "TestWrite");
+            }
+
+            CommandOutput results = regCommand.Execute(Parameters);
+            return results;
+        }
+
+
+        [TestMethod]
+        public void RegistryRead()
+        {
+            Dictionary<string, string> Parameters = new Dictionary<string, string>();
+            //Read this key: Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\SusClientId
+            //This method intentionally contains some duplicate code, to intentionally test just read - as some registries may be "readonly" due to permissions
+            List<Command> commands = Faction.Modules.Dotnet.Initialize.GetCommands();
+            Faction.Modules.Dotnet.Common.Command regCommand = commands[3];
+            Parameters.Add("Operation", "read");
+            Parameters.Add("Path", "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\DevicePath");
+            CommandOutput results = regCommand.Execute(Parameters);
+
+            //Sample output: C:\WINDOWS\inf
+            Assert.IsTrue(results.Complete);
+            Assert.IsTrue(results.Success);
+            Assert.IsTrue(results.Message.Contains("WINDOWS")); //inf SHOULD always be in Windows, consider a better directory in the future
+        }
+
+        [TestMethod]
+        public void RegistryWriteAndDelete()
+        {
+            CommandOutput results = KeyOperations("write", "HKEY_CURRENT_USER\\Environment\\TestValue", "TestWrite");
+            Assert.IsTrue(results.Complete);
+            Assert.IsTrue(results.Success);
+            results = KeyOperations("delete", "HKCU\\Environment\\TestValue", String.Empty);
+            Assert.IsTrue(results.Complete);
+            Assert.IsTrue(results.Success);
+        }
+    }
+}
