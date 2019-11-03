734b26dde953358e614e53d216cdf9781df05d54
Caleb McGary <intern4tional@users.noreply.github.com>
2019-11-02 17:28:25 -0700

    completes unload; tests for unload and scaffolding for tests

--- WinLib/WinLib/Commands/Driver.cs
+++ WinLib/WinLib/Commands/Driver.cs
@@ -42,7 +42,6 @@ namespace Faction.Modules.Dotnet.Commands
             SERVICE_PAUSED = 0x00000007
         }
 
-        [Flags]
         public enum SERVICE_ACCEPT : uint
         {
             STOP = 0x00000001,
@@ -67,6 +66,155 @@ namespace Faction.Modules.Dotnet.Commands
             public int waitHint;
         }
 
+        public enum SCM_ACCESS : uint
+        {
+            SC_MANAGER_CONNECT = 0x00001,
+            SC_MANAGER_CREATE_SERVICE = 0x00002,
+            SC_MANAGER_ENUMERATE_SERVICE = 0x00004,
+            SC_MANAGER_LOCK = 0x00008,
+            SC_MANAGER_QUERY_LOCK_STATUS = 0x00010,
+            SC_MANAGER_MODIFY_BOOT_CONFIG = 0x00020,
+            SC_MANAGER_ALL_ACCESS = ACCESS_MASK.STANDARD_RIGHTS_REQUIRED |
+                SC_MANAGER_CONNECT |
+                SC_MANAGER_CREATE_SERVICE |
+                SC_MANAGER_ENUMERATE_SERVICE |
+                SC_MANAGER_LOCK |
+                SC_MANAGER_QUERY_LOCK_STATUS |
+                SC_MANAGER_MODIFY_BOOT_CONFIG,
+            GENERIC_READ = ACCESS_MASK.STANDARD_RIGHTS_READ |
+                SC_MANAGER_ENUMERATE_SERVICE |
+                SC_MANAGER_QUERY_LOCK_STATUS,
+            GENERIC_WRITE = ACCESS_MASK.STANDARD_RIGHTS_WRITE |
+                SC_MANAGER_CREATE_SERVICE |
+                SC_MANAGER_MODIFY_BOOT_CONFIG,
+            GENERIC_EXECUTE = ACCESS_MASK.STANDARD_RIGHTS_EXECUTE |
+                SC_MANAGER_CONNECT | SC_MANAGER_LOCK,
+            GENERIC_ALL = SC_MANAGER_ALL_ACCESS,
+        }
+
+
+        public enum ACCESS_MASK : uint
+        {
+            DELETE = 0x00010000,
+            READ_CONTROL = 0x00020000,
+            WRITE_DAC = 0x00040000,
+            WRITE_OWNER = 0x00080000,
+            SYNCHRONIZE = 0x00100000,
+
+            STANDARD_RIGHTS_REQUIRED = 0x000F0000,
+
+            STANDARD_RIGHTS_READ = 0x00020000,
+            STANDARD_RIGHTS_WRITE = 0x00020000,
+            STANDARD_RIGHTS_EXECUTE = 0x00020000,
+
+            STANDARD_RIGHTS_ALL = 0x001F0000,
+
+            SPECIFIC_RIGHTS_ALL = 0x0000FFFF,
+
+            ACCESS_SYSTEM_SECURITY = 0x01000000,
+
+            MAXIMUM_ALLOWED = 0x02000000,
+
+            GENERIC_READ = 0x80000000,
+            GENERIC_WRITE = 0x40000000,
+            GENERIC_EXECUTE = 0x20000000,
+            GENERIC_ALL = 0x10000000,
+
+            DESKTOP_READOBJECTS = 0x00000001,
+            DESKTOP_CREATEWINDOW = 0x00000002,
+            DESKTOP_CREATEMENU = 0x00000004,
+            DESKTOP_HOOKCONTROL = 0x00000008,
+            DESKTOP_JOURNALRECORD = 0x00000010,
+            DESKTOP_JOURNALPLAYBACK = 0x00000020,
+            DESKTOP_ENUMERATE = 0x00000040,
+            DESKTOP_WRITEOBJECTS = 0x00000080,
+            DESKTOP_SWITCHDESKTOP = 0x00000100,
+
+            WINSTA_ENUMDESKTOPS = 0x00000001,
+            WINSTA_READATTRIBUTES = 0x00000002,
+            WINSTA_ACCESSCLIPBOARD = 0x00000004,
+            WINSTA_CREATEDESKTOP = 0x00000008,
+            WINSTA_WRITEATTRIBUTES = 0x00000010,
+            WINSTA_ACCESSGLOBALATOMS = 0x00000020,
+            WINSTA_EXITWINDOWS = 0x00000040,
+            WINSTA_ENUMERATE = 0x00000100,
+            WINSTA_READSCREEN = 0x00000200,
+
+            WINSTA_ALL_ACCESS = 0x0000037F
+        }
+
+        public enum SERVICE_ACCESS : uint
+        {
+            SERVICE_QUERY_CONFIG = 0x00001,
+            SERVICE_CHANGE_CONFIG = 0x00002,
+            SERVICE_QUERY_STATUS = 0x00004,
+            SERVICE_ENUMERATE_DEPENDENTS = 0x00008,
+            SERVICE_START = 0x00010,
+            SERVICE_STOP = 0x00020,
+            SERVICE_PAUSE_CONTINUE = 0x00040,
+            SERVICE_INTERROGATE = 0x00080,
+            SERVICE_USER_DEFINED_CONTROL = 0x00100,
+            SERVICE_ALL_ACCESS = (ACCESS_MASK.STANDARD_RIGHTS_REQUIRED |
+                SERVICE_QUERY_CONFIG |
+                SERVICE_CHANGE_CONFIG |
+                SERVICE_QUERY_STATUS |
+                SERVICE_ENUMERATE_DEPENDENTS |
+                SERVICE_START |
+                SERVICE_STOP |
+                SERVICE_PAUSE_CONTINUE |
+                SERVICE_INTERROGATE |
+                SERVICE_USER_DEFINED_CONTROL),
+
+            GENERIC_READ = ACCESS_MASK.STANDARD_RIGHTS_READ |
+                SERVICE_QUERY_CONFIG |
+                SERVICE_QUERY_STATUS |
+                SERVICE_INTERROGATE |
+                SERVICE_ENUMERATE_DEPENDENTS,
+
+            GENERIC_WRITE = ACCESS_MASK.STANDARD_RIGHTS_WRITE |
+                SERVICE_CHANGE_CONFIG,
+
+            GENERIC_EXECUTE = ACCESS_MASK.STANDARD_RIGHTS_EXECUTE |
+                SERVICE_START |
+                SERVICE_STOP |
+                SERVICE_PAUSE_CONTINUE |
+                SERVICE_USER_DEFINED_CONTROL,
+
+            ACCESS_SYSTEM_SECURITY = ACCESS_MASK.ACCESS_SYSTEM_SECURITY,
+            DELETE = ACCESS_MASK.DELETE,
+            READ_CONTROL = ACCESS_MASK.READ_CONTROL,
+            WRITE_DAC = ACCESS_MASK.WRITE_DAC,
+            WRITE_OWNER = ACCESS_MASK.WRITE_OWNER,
+        }
+
+        public enum SERVICE_ERROR
+        {
+#pragma warning disable CA1712 
+            SERVICE_ERROR_IGNORE = 0x00000000,
+            SERVICE_ERROR_NORMAL = 0x00000001,
+            SERVICE_ERROR_SEVERE = 0x00000002,
+            SERVICE_ERROR_CRITICAL = 0x00000003,
+#pragma warning restore CA1712
+        }
+
+        public enum SERVICE_TYPE : uint
+        {
+            SERVICE_KERNEL_DRIVER = 0x00000001,
+            SERVICE_FILE_SYSTEM_DRIVER = 0x00000002,
+            SERVICE_WIN32_OWN_PROCESS = 0x00000010,
+            SERVICE_WIN32_SHARE_PROCESS = 0x00000020,
+            SERVICE_INTERACTIVE_PROCESS = 0x00000100,
+        }
+
+        public enum SERVICE_START : uint
+        {
+            SERVICE_BOOT_START = 0x00000000,
+            SERVICE_SYSTEM_START = 0x00000001,
+            SERVICE_AUTO_START = 0x00000002,
+            SERVICE_DEMAND_START = 0x00000003,
+            SERVICE_DISABLED = 0x00000004,
+        }
+
 
         [DllImport("psapi.dll", CharSet = CharSet.Unicode, SetLastError = true)]
         private static extern bool EnumDeviceDrivers(
@@ -82,7 +230,7 @@ namespace Faction.Modules.Dotnet.Commands
             int baseNameStringSizeChars
         );
 
-        [DllImport("advapi32.dll", EntryPoint = "OpenSCManager", ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)]
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
         private static extern IntPtr OpenSCManager(
             string machineName,
             string databaseName,
@@ -135,14 +283,7 @@ namespace Faction.Modules.Dotnet.Commands
         private static extern bool CloseServiceHandle(
             IntPtr hSCObject);
 
-        /// <summary>
-        /// Enumerates the installed drivers; this is done via the Win32 method as it returns the actual sys files instead of just the service names.
-        /// The service controller method -> that can be used to return drivers if you call ServiceController.GetDevices but that does not return
-        /// the actual sys file that is associated with the driver; this method does this and is needed if you're looking to load or work with an installed sys
-        /// file via createfile and deviceiocontrol
-        /// </summary>
-        /// <param name="deviceDrivers"></param>
-        protected void EnumerateDeviceDrivers(List<string> deviceDrivers)
+        private void EnumerateDeviceDrivers(List<string> deviceDrivers)
         {
             bool success;
             UInt32 driverArraySize;
@@ -150,16 +291,13 @@ namespace Faction.Modules.Dotnet.Commands
             UIntPtr[] ddAddresses;
             UInt32 driverArrayBytesNeeded;
 
-            // Enumerate the installed drivers to see if this is already present
-            // https://docs.microsoft.com/en-us/windows/win32/psapi/enumerating-all-device-drivers-in-the-system
-
             success = EnumDeviceDrivers(null, 0, out driverArrayBytesNeeded);
-            
+
             if (!success)
             {
                 throw new ArgumentException("Call to EnumDeviceDrivers Failed.");
             }
-            
+
             if (driverArrayBytesNeeded == 0)
             {
                 throw new ArgumentException("No drivers found on system, possibly caught by a sandbox.");
@@ -170,7 +308,7 @@ namespace Faction.Modules.Dotnet.Commands
             ddAddresses = new UIntPtr[driverArraySize];
             success = EnumDeviceDrivers(ddAddresses, driverArraySizeBytes, out driverArrayBytesNeeded);
 
-            if(!success)
+            if (!success)
             {
                 throw new ArgumentException("Call to EnumDeviceDrivers Failed.");
             }
@@ -180,11 +318,99 @@ namespace Faction.Modules.Dotnet.Commands
             {
                 StringBuilder sb = new StringBuilder(1024);
                 int result = GetDeviceDriverFileName(ddAddresses[i], sb, sb.Capacity);
-                if (result!=0)
+                if (result != 0)
                 {
                     deviceDrivers.Add(sb.ToString());
                 }
             }
+        }
+
+        private bool InstallAndStartDriver(string driverPath, string serviceName)
+        {
+            IntPtr SC_MANAGER = IntPtr.Zero;
+            IntPtr SC_SERVICE = IntPtr.Zero;
+            try
+            {
+                SC_MANAGER = OpenSCManager(null, null, (uint)SCM_ACCESS.SC_MANAGER_CREATE_SERVICE);
+                SC_SERVICE = CreateService(
+                    SC_MANAGER,
+                    serviceName,
+                    serviceName,
+                    (uint)SERVICE_ACCESS.SERVICE_ALL_ACCESS, // Service All Access Permission
+                    (uint)SERVICE_TYPE.SERVICE_KERNEL_DRIVER, // Kernel Driver
+                    (uint)SERVICE_START.SERVICE_DEMAND_START, // Service Demand Start
+                    (uint)SERVICE_ERROR.SERVICE_ERROR_NORMAL, // Error Control - don't log the error TODO - fix
+                    driverPath,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null
+                    );
+                if (SC_SERVICE == null)
+                {
+                    throw new Exception("Failed to create service.");
+                }
+                if (!StartService(SC_SERVICE, 0, null))
+                {
+                    throw new Exception("Failed to start service.");
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (SC_SERVICE != IntPtr.Zero)
+                {
+                    CloseServiceHandle(SC_SERVICE);
+                }
+                if (SC_MANAGER != IntPtr.Zero)
+                {
+                    CloseServiceHandle(SC_MANAGER);
+                }
+            }
+
+            return true;
+        }
+
+        private bool StopAndUnloadDriver(string serviceName)
+        {
+            IntPtr SC_MANAGER = IntPtr.Zero;
+            IntPtr SC_SERVICE = IntPtr.Zero;
+            try
+            {
+                SC_MANAGER = OpenSCManager(null, null, (uint)SCM_ACCESS.SC_MANAGER_ALL_ACCESS);
+                SC_SERVICE = OpenService(SC_MANAGER, serviceName, (uint) SERVICE_ACCESS.SERVICE_STOP | (uint) SERVICE_ACCESS.DELETE | (uint)SERVICE_ACCESS.SERVICE_STOP);
+                if(SC_SERVICE == IntPtr.Zero)
+                {
+                    throw new Exception("Unable to get a handle on service.");
+                }
+                SERVICE_STATUS serviceStatus = new SERVICE_STATUS();
+                ControlService(SC_SERVICE, (uint)SERVICE_CONTROL.STOP, ref serviceStatus);
+                if (!DeleteService(SC_SERVICE))
+                {
+                    throw new Exception("Unable to delete service.");
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (SC_SERVICE != IntPtr.Zero)
+                {
+                    CloseServiceHandle(SC_SERVICE);
+                }
+                if (SC_MANAGER != IntPtr.Zero)
+                {
+                    CloseServiceHandle(SC_MANAGER);
+                }
+            }
+
+            return true;
         }
 
         private void DriverPathValidation(string driverPath)
@@ -222,8 +448,7 @@ namespace Faction.Modules.Dotnet.Commands
                 output.Message = "Insufficent privileges to execute module.";
                 return output;
             }
-            IntPtr SC_MANAGER = IntPtr.Zero;
-            IntPtr SC_SERVICE = IntPtr.Zero;
+
             try
             {
                 string operation = "Enumerate"; //set a default to Enumerate
@@ -239,12 +464,11 @@ namespace Faction.Modules.Dotnet.Commands
                         output.Message = JsonConvert.SerializeObject(deviceDrivers);
                         output.Success = true;
                         break;
-                    case "InstallAndStart":
+                    case "Install":
                         driverPath = Parameters["DriverPath"];
                         serviceName = Parameters["ServiceName"];
                         output.Message = "";
-                        // parameter validation
-                        if(driverPath == string.Empty)
+                        if (driverPath == string.Empty)
                         {
                             throw new ArgumentException("DriverPath is empty.");
                         }
@@ -252,37 +476,8 @@ namespace Faction.Modules.Dotnet.Commands
                         {
                             throw new ArgumentException("ServiceName is empty.");
                         }
-
                         DriverPathValidation(driverPath);
-
-                        SC_MANAGER = OpenSCManager(null, null, 0xF003F);
-                        SC_SERVICE = CreateService(
-                            SC_MANAGER,
-                            serviceName,
-                            serviceName,
-                            0xF003F, // Service All Access Permission
-                            0x00000001, // Kernel Driver
-                            0x00000003, // Service Demand Start
-                            0x00000000, // Error Control - don't log the error
-                            driverPath,
-                            null,
-                            null,
-                            null,
-                            null,
-                            null
-                            );
-
-                        // Next we should set the security for the kernel driver; this is intentionally not done as we do not know all the requirements
-                        // This potentially adds a risk if the driver is left present for an extended period of time.
-                        if (!StartService(SC_SERVICE, 0, null))
-                        {
-                            output.Message = "Service failed to start.";
-                            output.Success = false;
-                        }
-                        else
-                        {
-                            output.Success = true;
-                        }
+                        output.Success = InstallAndStartDriver(driverPath, serviceName);
                         break;
                     case "Call":
                         driverPath = Parameters["DriverPath"];
@@ -291,15 +486,7 @@ namespace Faction.Modules.Dotnet.Commands
                         break;
                     case "Unload":
                         serviceName = Parameters["ServiceName"];
-                        SC_MANAGER = OpenSCManager(null, null, 0x00001);
-                        SC_SERVICE = OpenService(SC_MANAGER, serviceName, 0x0020 | 0x10000);
-                        SERVICE_STATUS serviceStatus = new SERVICE_STATUS();
-                        ControlService(SC_SERVICE, 0x00000001, ref serviceStatus);
-                        if(!DeleteService(SC_SERVICE))
-                        {
-                            output.Message = "Unable to delete service.";
-                            output.Success = false;
-                        }
+                        output.Success = StopAndUnloadDriver(serviceName);
                         break;
                     default:
                         break;
@@ -310,38 +497,8 @@ namespace Faction.Modules.Dotnet.Commands
                 output.Message = e.Message;
                 output.Success = false;
             }
-            finally
-            {
-                if (SC_SERVICE != IntPtr.Zero)
-                {
-                    CloseServiceHandle(SC_SERVICE);
-                }
-                if (SC_MANAGER != IntPtr.Zero)
-                {
-                    CloseServiceHandle(SC_MANAGER);
-                }
-            }
-
             output.Complete = true;
             return output;
-        }
-
-        private string LoadAndCallDriver(string driverPath, string action)
-        {
-            //step 1: make sure that the driver is in C:\\Windows\\System32\\drivers
-
-            string results = "";
-            try
-            {
-                // https://docs.microsoft.com/en-us/windows/win32/fileio/creating-and-opening-files
-                IntPtr fileHandle = CreateFile(driverPath, FileAccess.ReadWrite, FileShare.ReadWrite, IntPtr.Zero, FileMode.Open, FileAttributes.Normal, IntPtr.Zero);
-            }
-            catch (FileNotFoundException ex)
-            {
-                throw ex;
-            }
-
-            return results;
         }
 
         /// <summary>
--- WinLib/WinLibTests/DriverTests.cs
+++ WinLib/WinLibTests/DriverTests.cs
@@ -4,12 +4,53 @@ using System;
 using System.Diagnostics;
 using System.Collections.Generic;
 using Microsoft.Win32;
+using System.Net;
+using System.IO;
+using System.Security.Principal;
 
 namespace WinLibTests
 {
     [TestClass]
     public class DriverTests
     {
+
+        // Scaffolding Methods
+        private bool PermissionCheck()
+        {
+            bool isElevated;
+            List<string> deviceDrivers = new List<string>();
+
+            // Check privileges, admin is required to work with drivers for this module
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                isElevated = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+
+            return isElevated;
+        }
+
+        private string DriverSetup()
+        {
+            // This method has some scaffolding in that we download a file, it will require internet to run
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            string tempPath = System.IO.Path.GetTempPath();
+            string processhacker = string.Empty;
+            WebClient webClient = new WebClient();
+            if (Environment.Is64BitOperatingSystem)
+            {
+                processhacker = "https://github.com/processhacker/processhacker/raw/master/KProcessHacker/bin-signed/amd64/kprocesshacker.sys";
+            }
+            else
+            {
+                processhacker = "https://github.com/processhacker/processhacker/raw/master/KProcessHacker/bin-signed/i386/kprocesshacker.sys";
+            }
+            tempPath += "\\kprocesshacker.sys"; //use this path for tests, could change it f
+            webClient.DownloadFile(processhacker, tempPath);
+
+            return tempPath;
+        }
+
         [TestMethod]
         public void DriverInitialization()
         {
@@ -36,14 +77,57 @@ namespace WinLibTests
         [TestMethod]
         public void DriverInstall()
         {
+            Assert.IsTrue(PermissionCheck());
+            string tempPath = DriverSetup();
             Dictionary<string, string> Parameters = new Dictionary<string, string>();
             List<Command> commands = Faction.Modules.Dotnet.Initialize.GetCommands();
             Faction.Modules.Dotnet.Common.Command driverCommand = commands[6];
-            Parameters.Add("Operation", "InstallAndStart");
-            Parameters.Add("DriverPath", "D:\\temp\\kprocesshacker.sys");
+            Parameters.Add("Operation", "Install");
+            Parameters.Add("DriverPath", tempPath);
             Parameters.Add("ServiceName", "ProcessHacker");
             CommandOutput results = driverCommand.Execute(Parameters);
             Console.WriteLine(results.Message);
+            File.Delete(tempPath);
+            Assert.AreEqual("driver", driverCommand.Name);
+            Assert.IsTrue(results.Complete);
+            Assert.IsTrue(results.Success);
+        }
+
+        [TestMethod]
+        public void DriverUnload()
+        {
+            // sc create ProcessHacker binPath="D:\temp\kprocesshacker.sys" type=kernel start=demand
+            // sc start ProcessHacker
+            Assert.IsTrue(PermissionCheck());
+            string tempPath = DriverSetup();
+            System.Diagnostics.Process createService = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C sc create ProcessHacker binPath=" + tempPath + " type=kernel start=demand";
+            createService.StartInfo = startInfo;
+            createService.Start();
+            createService.WaitForExit(2000);
+            createService.Close();
+
+            System.Diagnostics.Process startService = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startServiceProcessInfo = new System.Diagnostics.ProcessStartInfo();
+            startServiceProcessInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startServiceProcessInfo.FileName = "cmd.exe";
+            startServiceProcessInfo.Arguments = "/C sc start \"ProcessHacker\"";
+            startService.StartInfo = startServiceProcessInfo;
+            startService.Start();
+            startService.WaitForExit(2000);
+            startService.Close();
+
+            Dictionary<string, string> Parameters = new Dictionary<string, string>();
+            List<Command> commands = Faction.Modules.Dotnet.Initialize.GetCommands();
+            Faction.Modules.Dotnet.Common.Command driverCommand = commands[6];
+            Parameters.Add("Operation", "Unload");
+            Parameters.Add("ServiceName", "ProcessHacker");
+            CommandOutput results = driverCommand.Execute(Parameters);
+            Console.WriteLine(results.Message);
+            File.Delete(tempPath);
             Assert.AreEqual("driver", driverCommand.Name);
             Assert.IsTrue(results.Complete);
             Assert.IsTrue(results.Success);
